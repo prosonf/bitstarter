5ec2e2e721a09de960b149d275deab6e336262ce
pedro <carulli06@gmail.com>
2013-08-05 16:33:51 +0200

    Support for CORS

--- web.js
+++ web.js
@@ -3,9 +3,31 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+// http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'cool beans' }));
+    app.use(express.methodOverride());
+    app.use(allowCrossDomain);
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
 app.get('/', function(request, response) {
 	var buf = fs.readFileSync('index.html');
   	response.send(buf.toString());
+
+  	next();
 });
 
 var port = process.env.PORT || 8080;
